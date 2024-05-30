f87dda9d24b3bf1fd60f5df948537f9b17730f2d
GrantFinnegan <grantfinnegan@gmail.com>
2024-05-30 15:44:04 -0500

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    if (countArg == 0) return 0;
+    if (countArg < 0) return 'OOPS';
 
+    let previousNumber = 0;
+    let currentNumber = 1;
+    for (let i = 1; i < countArg; i++) {
+        let nextNumber;
+        nextNumber = currentNumber + previousNumber;
+        previousNumber = currentNumber;
+        currentNumber = nextNumber;
+    }
+
+    return currentNumber;
 };
 
 // Do not edit below this line
