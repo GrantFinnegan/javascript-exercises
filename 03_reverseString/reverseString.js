41496dbb7207170f21eb47765d9ee0f3b072cc5c
GrantFinnegan <grantfinnegan@gmail.com>
2024-05-30 11:30:11 -0500

    complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse = "") {
+    let outputString = '';
 
+    for (let i = 0; i < stringToReverse.length; i++) {
+        outputString = stringToReverse.at(i) + outputString;
+    }
+
+    return outputString;
 };
 
 // Do not edit below this line
