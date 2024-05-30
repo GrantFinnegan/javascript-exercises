0ad26ad30590d00cfe0981a897ef759f2bcdbe78
GrantFinnegan <grantfinnegan@gmail.com>
2024-05-30 16:22:48 -0500

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
 
+    function getAge(person){
+        return person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : (new Date()).getFullYear() - person.yearOfBirth;
+    }
+
+    function findOldestPerson(oldestPerson, nextPerson){
+        if ( getAge(oldestPerson) > getAge(nextPerson)) {
+            return oldestPerson;
+        }
+        else return nextPerson;
+    }
+
+    return peopleArray.reduce(findOldestPerson);
 };
 
 // Do not edit below this line
