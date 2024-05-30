0ee64c6a7fbfef12313c6aa4f338b87f283b875f
GrantFinnegan <grantfinnegan@gmail.com>
2024-05-30 13:15:22 -0500

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (element of array) {
+    total += element;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (element of array) {
+    product *= element;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let product = 1;
+  for (let i = 1; i <= number; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
