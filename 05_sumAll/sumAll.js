d0dc4f5f4c73b1b0036bf5a679d12cb22d6150c5
GrantFinnegan <grantfinnegan@gmail.com>
2024-05-30 12:44:22 -0500

    complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if(firstNumber < 0 ||secondNumber < 0) {
+        return 'ERROR';
+    }
+    if(typeof(firstNumber) != 'number' || typeof(secondNumber) != 'number') {
+        return 'ERROR';
+    }
+    
+    let sum = 0;
+    let min = Math.min(firstNumber, secondNumber);
+    let max = Math.max(firstNumber, secondNumber);
+    for (let i = min; i <= max; i ++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
