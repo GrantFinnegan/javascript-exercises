83034bb6b59765ae89523997f3ab85719e000612
GrantFinnegan <grantfinnegan@gmail.com>
2024-05-30 15:31:43 -0500

    complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (phrase = "") {
+    const CHARACTER_ALLOWLIST = '0123456789abcdefghijklmnopqrstuvwxyz';
 
+    //clean up capitalization, whitespace, and punctuation
+    phrase = phrase.toLowerCase();
+
+    let cleanedPhrase = "";
+    let cleanedReversedPhrase = "";
+    for (let i = 0; i < phrase.length; i++) {
+        if (CHARACTER_ALLOWLIST.includes(phrase.at(i))) {
+            cleanedPhrase = cleanedPhrase + phrase.at(i); //append
+            cleanedReversedPhrase = phrase.at(i) + cleanedReversedPhrase; //append in reverse
+        }
+    }
+    return cleanedPhrase === cleanedReversedPhrase;
 };
 
 // Do not edit below this line
